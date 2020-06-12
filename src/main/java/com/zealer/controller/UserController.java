93cfb2e862d8615d4f3c5178582c82a94a40b480
yaowenyu0927 <854776263@qq.com>
2020-06-12 16:52:30 +0800

    姚文宇zealer

--- src/main/java/com/zealer/controller/UserController.java
+++ src/main/java/com/zealer/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.zealer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,5 +20,18 @@ public class UserController {
         modelAndView.setViewName("new");
         modelAndView.addObject("nums","100");
         return modelAndView;
+    }
+
+    @RequestMapping(value="/login",produces = "text/plain;charset=utf-8")
+    @ResponseBody
+    @CrossOrigin(origins = "*")
+
+    public String toLogin(String  uname,String pwd){
+        System.out.println(uname+","+pwd);
+        if(uname.equals("zhangsan")&&pwd.equals("123")){
+            return "登录成功";
+        }else {
+            return "登录失败";
+        }
     }
 }
